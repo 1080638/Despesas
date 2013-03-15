6ef7badd1ade1f571bfa0164a4733de03e6ffaff
JoaoCochofel <i060721@isep.ipp.pt>
2013-03-15 20:01:01 +0000

    Ja tem mais cenas, e ta bue de fixe!

--- eapli teste/src/eapli/teste/MenuDespesas.java
+++ eapli teste/src/eapli/teste/MenuDespesas.java
@@ -4,12 +4,22 @@
  */
 package eapli.teste;
 
+import java.util.Scanner;
+
 /**
  *
  * @author João
  */
 public class MenuDespesas {
+    private float valor;
+    private String descricao;
+    private Scanner in= new Scanner(System.in);
+    
     public MenuDespesas(){
+        System.out.println("Insira o valor da Despesa:\n");
+        valor = in.nextFloat();
+        System.out.println("Insira a Descrição");
+        descricao = in.next();
         
     }
 }
